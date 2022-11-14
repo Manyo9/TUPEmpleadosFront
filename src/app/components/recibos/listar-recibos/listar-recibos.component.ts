ee09c86417ecff595b7ea1bc50343339fe121220
Manyo <agusmangini@hotmail.com>
2022-11-14 17:02:17 -0300

    implemento listado recibos por legajo

--- src/app/components/recibos/listar-recibos/listar-recibos.component.ts
+++ src/app/components/recibos/listar-recibos/listar-recibos.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { ReciboSueldoNetoDTO } from 'src/app/models/ReciboSueldoNetoDTO';
 import { EmpleadoService } from 'src/app/services/empleado.service';
+import { SweetAlert } from 'sweetalert/typings/core';
+const swal: SweetAlert = require('sweetalert');
 
 @Component({
   selector: 'app-listar-recibos',
@@ -11,7 +14,7 @@ import { EmpleadoService } from 'src/app/services/empleado.service';
 export class ListarRecibosComponent implements OnInit {
 
   formulario: FormGroup;
-  listadoRecibos: ReciboSueldoNetoDTO[];
+  $listadoRecibos: Observable<ReciboSueldoNetoDTO[]>;
   constructor(
     private empleadoService : EmpleadoService,
     private formBuilder : FormBuilder
@@ -24,10 +27,18 @@ export class ListarRecibosComponent implements OnInit {
   }
 
   buscar(): void {
-    alert('todo')
+    if(this.formulario.invalid){
+      swal({ title: 'Atención!', text: `Ingresá un legajo antes de buscar.`, icon: 'warning' });
+      return;
+    }
+    this.$listadoRecibos =  this.empleadoService.obtenerRecibosPorLegajo(this.valueLegajo);
   }
 
   get controlLegajo(): FormControl {
     return this.formulario.controls['legajo'] as FormControl;
+  }
+
+  get valueLegajo(): number {
+    return this.formulario.get('legajo')?.value;
   }
 }
