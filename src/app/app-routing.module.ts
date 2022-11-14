4e6956f87e0aa5ba4cc5abd585533d46f981bb81
Manyo <agusmangini@hotmail.com>
2022-11-14 15:46:16 -0300

    navbar y routing list y alta recibos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AltaEmpleadoComponent } from './components/empleados/alta-empleado/alta-empleado.component';
 import { ListadoEmpleadosComponent } from './components/empleados/listado-empleados/listado-empleados.component';
 import { HomeComponent } from './components/home/home.component';
+import { AltaReciboComponent } from './components/recibos/alta-recibo/alta-recibo.component';
+import { ListarRecibosComponent } from './components/recibos/listar-recibos/listar-recibos.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'empleados/listado', component: ListadoEmpleadosComponent },
   { path: 'empleados/nuevo', component: AltaEmpleadoComponent },
+  { path: 'recibos/listado', component: ListarRecibosComponent },
+  { path: 'recibos/nuevo', component: AltaReciboComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
 
